a55fe1bf891498f106ff20f2fa00748e229fd0d0
GonzaloVillagra <Gonzalo.villagra.f@gmail.com>
2025-11-21 17:58:16 -0300

    steps prueba de login

--- src/test/java/com/empresa/LoginSteps.java
+++ src/test/java/com/empresa/LoginSteps.java
@@ -1,28 +1,25 @@
 package com.empresa;
 
 import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.junit.jupiter.api.Test;
-
+import io.cucumber.java.en.Then;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LoginSteps {
 
-    @Given("estoy en la pagina de login")
-    public void estoy_pagina_inicio(){
-        System.out.println("--- [STEP] Abriendo pagina de login ---");
+    @Given("que estoy en la pagina de login")
+    public void que_estoy_en_la_pagina_de_login() {
+        System.out.println("--- [STEP] Abriendo navegador en Login ---");
     }
 
-    @When("Ingreso del usuario {string} y clave {string}")
-    public void ingreso_usuario_clave(String nombre, String clave){
-        System.out.println("--- [STEP] Ingresando Usuario ---");
+    @When("ingreso usuario {string} y clave {string}")
+    public void ingreso_usuario_y_clave(String usuario, String clave) {
+        System.out.println("--- [STEP] Ingresando usuario: " + usuario + " ---");
     }
 
-    @Then("deberia ver pagina de inicio")
-    public void deberia_pagina_inicio(){
-        System.out.println("--- [STEP] deberia ver la pagina de inicio --- ");
+    @Then("deberia ver la pagina de inicio")
+    public void deberia_ver_la_pagina_de_inicio() {
+        System.out.println("--- [STEP] Validando pagina de inicio ---");
         assertTrue(true);
     }
 }
-
